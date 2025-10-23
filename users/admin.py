f91ccd4459a17e2ca374c2f20c26386381b8afce
Sebas16608 <asebasrr444@gmail.com>
2025-10-22 18:36:13 -0600

    Creando el admin de la app users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import UserProfile
 
-# Register your models here.
+# Inline para mostrar el perfil dentro del usuario
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fields = ('phone', 'avatar', 'newsletter_subscription', 'bio')
+
+# Extender el UserAdmin de Django
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserProfileInline,)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_phone')
+    
+    def get_phone(self, obj):
+        return obj.profile.phone if hasattr(obj, 'profile') else '-'
+    get_phone.short_description = 'Teléfono'
+
+# Re-registrar UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone', 'newsletter_subscription', 'created_at')
+    list_filter = ('newsletter_subscription', 'created_at')
+    search_fields = ('user__username', 'user__email', 'phone')

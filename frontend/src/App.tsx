50ca44723d50caf52213bae6114b1b62993f2911
arodriguez2019 <auky100@gmail.com>
2026-05-11 23:59:48 -0600

    se hace el ultimo push del dia 11/5/2025

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,8 +14,8 @@ function App() {
         <p>muestra simple de datos</p>
 
       {/* aqui agrego productos ya despues se puede cambiar de lugar ya que es un llamado de componente ProductoCard ojo que ahi pondre un ngs o bien dependiendo*/}
-
-        <div>
+      {/* ojo hice aqui la herencia de atributos por que soy huevon */}
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-6 px-3">
 
           <ProductoCard />
           <ProductoCard />
--- frontend/src/components/ProductoCard.tsx
+++ frontend/src/components/ProductoCard.tsx
@@ -4,13 +4,19 @@ function ProductoCard(){
     // por el momento son datos mock osea datos falsos
     return(
 
-        <div>
+        <div className="border rounded-xl p-4 shadow-md">
 
-            <h2>Cocodrilo grandote</h2>
-            <p>a este cocodrilo le gusta comer niños</p>
-            <p>Q30,000.00</p>
 
-            <button>agregar</button>
+    {/* descubri algo interesante text-xl es para texto grandote y xs es para pequeño */}
+    {/* lo dejo en mt1 por el momento para imagen si habra que ponerle tal vez un espaciado de 3 o 2 */}
+         
+            <h2 className="text-xl font-semibold mt-1"> Cocodrilo grandote </h2>
+            <p className="text-gray-900"> a este cocodrilo le gusta comer niños we </p>
+            <p className="text-lg mt-0.5"> Q30,000.00 </p>
+
+
+        {/* el gray y white casi son lo mismo casi */}
+            <button className="mt-4 w-full bg-black text-white py-1.5 rounded-2xl hover:bg-neutral-900">agregar</button>
 
         </div>
 

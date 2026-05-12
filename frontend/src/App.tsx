57b3c814ab7638612ab33107a83cddf36bfbfb71
arodriguez2019 <auky100@gmail.com>
2026-05-11 23:30:25 -0600

    se empieza a construir la vista de productos para funcionamiento simple

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import ProductoCard from "./components/ProductoCard";
 
 function App() {
   
@@ -11,6 +12,16 @@ function App() {
 
         <h2>Bienvenidos etc etc</h2>
         <p>muestra simple de datos</p>
+
+      {/* aqui agrego productos ya despues se puede cambiar de lugar ya que es un llamado de componente ProductoCard ojo que ahi pondre un ngs o bien dependiendo*/}
+
+        <div>
+
+          <ProductoCard />
+          <ProductoCard />
+          <ProductoCard />
+
+        </div>
 
       </main>
     </>
---
+++ frontend/src/components/ProductoCard.tsx
@@ -0,0 +1,24 @@
+function ProductoCard(){
+
+
+    // por el momento son datos mock osea datos falsos
+    return(
+
+        <div>
+
+            <h2>Cocodrilo grandote</h2>
+            <p>a este cocodrilo le gusta comer niños</p>
+            <p>Q30,000.00</p>
+
+            <button>agregar</button>
+
+        </div>
+
+    );
+
+
+
+}
+
+
+export default ProductoCard;
